ea7b2e329f182970d8f809bdfa9a24f030a9eb89
Amy C <missch3n@gmail.com>
2019-10-07 20:55:23 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  numbers = [1,2,3]
+  new_numbers = [9,10,16,25]
+
+  while numbers[counter]
+  numbers.each do |a|
+  new_numbers.each do |a|
+    a**=2
+  end
 end
